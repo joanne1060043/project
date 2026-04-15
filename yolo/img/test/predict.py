f9966d0efc4466506f72433e44b1a16abd614d69
joanne1060043 <joanne1060043@gmail.com>
2026-04-16 00:17:07 +0800

    456

---
+++ yolo/img/test/predict.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# 載入訓練好的模型
+model = YOLO('best.pt')
+
+# 設定圖片路徑，這裡假設您有一張圖片 'image.jpg'
+results = model('image.jpg')  # 輸入圖片路徑
+result = results[0]
+# 顯示結果
+result.show()  # 顯示圖片上的預測結果
+
+# 若需要保存預測結果，可以使用：
+result.save()  # 將結果保存到指定文件夾
