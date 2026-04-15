46bb4abf41be8377879cae46bbd0ef071f7cbbb5
joanne1060043 <joanne1060043@gmail.com>
2026-04-15 22:12:27 +0800

    123

--- download_data.py
+++
@@ -1,9 +0,0 @@
-from roboflow import Roboflow
-
-# 初始化 Roboflow
-rf = Roboflow(api_key="yqpOaFUOdnjm4NLwRSyp")
-project = rf.workspace("-suqln").project(" ")
-
-# 下載數據集到本地
-# 它會自動幫您建立一個資料夾並放好 images 和 labels
-dataset = project.version(1).download("yolov11")
