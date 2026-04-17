6177634047c566daf9c0d46a08a525aabd704016
joanne1060043 <joanne1060043@gmail.com>
2026-04-17 23:41:12 +0800

    4/17訓練完成

---
+++ cv.py
@@ -0,0 +1,43 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO(r"Drone-detectionn.v1i.yolov8\predict\best.pt")
+
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+
+# 設定攝影機解析度
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+# 建立可縮放視窗
+cv2.namedWindow("YOLO Camera", cv2.WINDOW_NORMAL)
+
+# 最大化視窗
+cv2.setWindowProperty(
+    "YOLO Camera",
+    cv2.WND_PROP_FULLSCREEN,
+    cv2.WINDOW_NORMAL
+)
+
+# 設定初始大小（可調整）
+cv2.resizeWindow("YOLO Camera", 1600, 900)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("無法讀取影像")
+        break
+
+    results = model(frame, conf=0.5)
+    r = results[0]
+
+    annotated_frame = r.plot()
+
+    cv2.imshow("YOLO Camera", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- train.py
+++ train.py
@@ -4,12 +4,10 @@ def main():
     model = YOLO("yolo11n.pt")
 
     model.train(
-        data="Drone-detection.v1i.yolov8/data.yaml",  # 修正這裡
+        data="Drone-detectionn.v1i.yolov8/data.yaml",  # 修正這裡
         epochs=50,
         imgsz=640,
-        batch=16,
-        device=0,
-        workers=0
+        resume=True
     )
 
 if __name__ == "__main__":
