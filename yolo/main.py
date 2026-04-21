e5d978d3ede22a86fc37e05b19a672f8a88085d2
PoChieh-workplace <justin07249495@gmail.com>
2026-04-22 02:13:40 +0800

    5080 model test

--- yolo/main.py
+++ yolo/main.py
@@ -7,11 +7,12 @@ def train_drone_model():
     # 2. 開始訓練
     results = model.train(
         data='data.yaml',        # 指定你的設定檔
-        epochs=10,               # 訓練輪數 (可根據需求調整)
-        imgsz=640,               # 影像大小
+        epochs=20,               # 訓練輪數 (可根據需求調整)
+        imgsz=960,               # 影像大小
         device='0',              # 使用第一個 GPU
         project='drone_project', # 輸出的資料夾名稱
         name='v11_experiment',   # 實驗名稱
+        batch=32,                # GPU 每次看張
         resume=True              # 如果已經中斷過，可以選擇恢復訓練
     )
 
